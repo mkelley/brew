e1503a6bf2d1b77f7973afada532c719df48948a
Michael S. P. Kelley <msk@astro.umd.edu>
2016-04-19 08:53:54 -0400

    Add final_gravity, find_yeast, yeast_data

--- brew/fermentation.py
+++ brew/fermentation.py
@@ -6,32 +6,36 @@ fermentation --- Yeast and fermentations.
 
 """
 
-def real_extract(og, fg):
-    """Real extract (degrees Plato) from original and final gravity.
-
-    Brew by the Numbers, Hall, Zymergy, Summer 1995.
-
-    """
-
-    from .util import sg2plato
-
-    oe = sg2plato(og)
-    ae = sg2plato(fg)
-    q = 0.22 + 0.001 * oe
-    return (q * oe + ae) / (1 + q)
-
-def real_attenuation(og, fg):
-    """Real attenuation (%) from original and final gravity.
-
-    Brew by the Numbers, Hall, Zymergy, Summer 1995.
-
-    """
-
-    from .util import sg2plato
-
-    oe = sg2plato(og)
-    re = real_extract(og, fg)
-    return (oe - re) / oe * 100.
+# key, (name, min, max apparent attenutation)
+yeast_data = {
+    'WLP001': ('California Ale', 73, 80),
+    'WLP002': ('English Ale', 63, 70),
+    'WLP004': ('Irish Ale', 69, 74),
+    'WLP051': ('California Ale V', 70, 75),
+    'WLP072': ('French Ale', 68, 75),
+    'WLP080': ('Cream Ale Blend', 75, 80),
+    'WLP300': ('Hefeweizen', 72, 76),
+    'WLP400': ('Belgian Wit', 74, 78),
+    'WLP500': ('Monastery Ale', 75, 80),
+    'WLP530': ('Abbey Ale', 75, 80),
+    'WLP550': ('Belgian Ale', 78, 85),
+    'WLP565': ('Belgian Saison I', 65, 75),
+    'WLP566': ('Belgian Saison II', 78, 85),
+    'WLP568': ('Belgian Style Saison', 70, 80),
+    'WLP570': ('Belgian Golden Ale', 73, 78),
+    'WLP585': ('Belgian Saison III', 70, 74),
+    'WLP590': ('French Saison', 73, 80),
+    'WLP810': ('San Francisco Lager', 65, 70),
+    'WLP820': ('Oktoberfest/Märzen Lager', 65, 73),
+    'WLP644': ('Sacchromyces bruxellensis Trois', 85, 100),
+    'WLP645': ('Brettanomyces claussenii', 85, 100),
+    'WLP648': ('Brettanomyces bruxellensis Trois Vrai', 85, 100),
+    'WLP650': ('Brettanomyces bruxellensis', 85, 100),
+    'WLP650': ('Brettanomyces lambicus', 85, 100),
+    'WLP655': ('Sour Mix 1', 85, 100),
+    'WLP665': ('Flemish Ale Blend', 80, 100),
+    'WLP670': ('American Farmhouse Blend', 75, 82),
+}
 
 def abv(og, fg):
     """Alcohol by volume.
@@ -46,6 +50,53 @@ def abv(og, fg):
     re = real_extract(og, fg)
     abw = (oe - re) / (2.0665 - 0.010665 * oe)
     return abw * fg / 0.794
+
+def final_gravity(sg, T_mash, yeast):
+    """Estimate final gravity.
+    
+    Parameters
+    ----------
+    sg : float
+      Starting gravity, without any 100% fermentables.
+    T_mash : float
+      Mash temperature.
+    yeast : string
+      Name of a yeast strain.
+
+    Returns
+    -------
+    fg : float
+    app_atten : float
+
+    """
+    try:
+        product, name, atten = find_yeast(yeast)
+    except:
+        print("Yeast not found.  Valid values:")
+        for k, v in _yeast.items():
+            print('"{}" / "{}"'.format(k, v[0]))
+        raise
+
+    dT = T_mash - 152
+    a = sum(atten) / 2 - dT
+
+    return sg - (sg - 1) * a / 100, a
+
+def find_yeast(yeast):
+    """Find yeast data by name or product key."""
+
+    if yeast in _yeast:
+        product = yeast
+    else:
+        for k, v in _yeast.items():
+            if yeast == v[0]:
+                product = k
+                break
+        else:
+            raise KeyError(yeast)
+
+    name, atten_min, atten_max = _yeast[product]
+    return product, name, (atten_min, atten_max)
 
 def priming_sugar(T, r, v):
     """Weight of corn sugar for priming.
@@ -80,4 +131,31 @@ def priming_sugar(T, r, v):
     """
 
     return (0.5360 * v) *  ((r - 3.0378) + (0.050 * T) - (0.0002655 * T**2))
+
+def real_attenuation(og, fg):
+    """Real attenuation (%) from original and final gravity.
+
+    Brew by the Numbers, Hall, Zymergy, Summer 1995.
+
+    """
+
+    from .util import sg2plato
+
+    oe = sg2plato(og)
+    re = real_extract(og, fg)
+    return (oe - re) / oe * 100.
+
+def real_extract(og, fg):
+    """Real extract (degrees Plato) from original and final gravity.
+
+    Brew by the Numbers, Hall, Zymergy, Summer 1995.
+
+    """
+
+    from .util import sg2plato
+
+    oe = sg2plato(og)
+    ae = sg2plato(fg)
+    q = 0.22 + 0.001 * oe
+    return (q * oe + ae) / (1 + q)
 
