951cd3938c676717392d829cf6885df9c35aa74c
Michael S. P. Kelley <msk@astro.umd.edu>
2017-02-03 20:24:44 -0500

    Beefier example

--- examples/example.py
+++ examples/example.py
@@ -1,13 +1,33 @@
 from brew import *
-from brew.timing import Boil
-from brew import *
-from brew.timing import Boil
+from brew.timing import Boil, HopStand, Secondary
 
-wort = Wort([Fermentable(PPG.AmericanTwoRow, 10),
-             Hop('Cascade', 7.0, 1.0, Boil(60)),
-             Hop('Cascade', 7.0, 0.5, Boil(30)),
-             Hop('Cascade', 7.0, 0.5, Boil(10))
+print('=' * 80)
+wort = Wort([Fermentable(PPG.AmericanTwoRow, 4.5),
+             Fermentable(PPG.WheatMalt, 4.125, name='White wheat malt'),
+             Fermentable(PPG.AmericanCaramel20, 6 / 16, name='Caramel Vienne malt'),
+             Fermentable(PPG.AcidMalt, 2.5 / 16, name='White wheat malt'),
+             Hop('Magnum', 11.5, 0.6, Boil(60)),
+             Hop('Amarillo', 8.5, 0.3, HopStand(30)),
+             Hop('Citra', 13.4, 0.7, HopStand(30)),
+             Hop('Amarillo', 8.5, 1.0, Secondary(7)),
+             Hop('Citra', 13.4, 3.5, Secondary(7)),
              ])
+yeast = Culture(CultureBank.CaliforniaAle)
+brew = Brew(wort, yeast)
+beer = brew.brew()
+brew.summary()
+beer.summary()
+
+wort = Wort([Fermentable(PPG.AmericanTwoRow, 4.5),
+             Fermentable(PPG.WheatMalt, 4.125, name='White wheat malt'),
+             Fermentable(PPG.AmericanCaramel20, 6 / 16, name='Caramel Vienne malt'),
+             Fermentable(PPG.AcidMalt, 2.5 / 16, name='White wheat malt'),
+             Hop('Magnum', 11.5, 0.7, Boil(60)),
+             Hop('Amarillo', 8.5, 1.3, HopStand(30)),
+             Hop('Citra', 13.4, 0.8, HopStand(30)),
+             Hop('Amarillo', 8.5, 0.7, Secondary(7)),
+             Hop('Citra', 13.4, 3.5, Secondary(7)),
+             ], volume=5.75, boil_time=70, efficiency=0.76)
 yeast = Culture(CultureBank.CaliforniaAle)
 brew = Brew(wort, yeast)
 beer = brew.brew()
